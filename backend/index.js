42b515dc4dd317f9456a10bd8d8a6443610c400f
pdev25214@gmail.com <pdev@DESKTOP-CF928S9.localdomain>
2026-02-06 10:45:58 +0000

    Add .gitignore and project files

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+const rootRouter = require('./routes/root')
+require('dotenv').config()
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use('/api/v1', rootRouter)
+
+app.listen(3000, () => console.log('Backend server running on port 3000'))
---
+++ backend/routes/root.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const rootRouter = express.Router()
+
+// import other routes here !!
+
+module.exports = rootRouter
